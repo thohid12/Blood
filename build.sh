37dc798ff501e04c3882dda5f72c0c2bdf36f6e1
thohid12 <145980877+thohid12@users.noreply.github.com>
2024-09-30 16:23:55 +0600

    Update build.sh for mikeration

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+manage.py makemigrations
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
