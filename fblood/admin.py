639aa38d2ca392f83bd521af4962cdebeb0afe3a
thohid12 <145980877+thohid12@users.noreply.github.com>
2024-09-30 15:19:29 +0600

    Update admin.py for feedback

--- fblood/admin.py
+++ fblood/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
  # Import the model you want to register
-from .models import Donar_donate_info,PreviousInfo,OTP
+from .models import Donar_donate_info,PreviousInfo,OTP,Feedback
 
 # Register your models here.
  
@@ -9,3 +9,4 @@ from .models import Donar_donate_info,PreviousInfo,OTP
 admin.site.register(Donar_donate_info)
 admin.site.register(PreviousInfo)
 admin.site.register(OTP)
+admin.site.register(Feedback)
